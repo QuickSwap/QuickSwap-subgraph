f9776eea916249cf5cf52539d97df8d58b80e510
Paul Barclay <paulbarclay@users.noreply.github.com>
2021-05-06 20:31:23 -0700

    Change whitelist check to internal

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -75,8 +75,8 @@ export function findEthPerToken(token: Token): BigDecimal {
   }
 
   // loop through whitelist and check if paired with any
-  for (let i = 0; i < WHITELIST.length; ++i) {
-    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+  for (let i = 0; i < token.whitelist.length; ++i) {
+    let pairAddress = token.whitelist[i]
     if (pairAddress.toHexString() != ADDRESS_ZERO) {
       let pair = Pair.load(pairAddress.toHexString())
       if (pair.token0 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
