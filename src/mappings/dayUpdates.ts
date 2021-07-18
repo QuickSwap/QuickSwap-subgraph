cd9d56ddea6e2f874547d94b188bc5755e254f81
sameepsi <sameepsi@gmail.com>
2021-07-18 15:32:47 +0530

    changes as per new graph version

--- src/mappings/dayUpdates.ts
+++ src/mappings/dayUpdates.ts
@@ -1,10 +1,10 @@
 import { PairHourData } from './../types/schema'
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, EthereumEvent } from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal, ethereum } from '@graphprotocol/graph-ts'
 import { Pair, Bundle, Token, UniswapFactory, UniswapDayData, PairDayData, TokenDayData } from '../types/schema'
 import { ONE_BI, ZERO_BD, ZERO_BI, FACTORY_ADDRESS } from './helpers'
 
-export function updateUniswapDayData(uniswap: UniswapFactory, event: EthereumEvent): UniswapDayData {
+export function updateUniswapDayData(uniswap: UniswapFactory, event: ethereum.Event): UniswapDayData {
   // let uniswap = UniswapFactory.load(FACTORY_ADDRESS)
   let timestamp = event.block.timestamp.toI32()
   let dayID = timestamp / 86400
@@ -28,7 +28,7 @@ export function updateUniswapDayData(uniswap: UniswapFactory, event: EthereumEve
   return uniswapDayData as UniswapDayData
 }
 
-export function updatePairDayData(pair: Pair, event: EthereumEvent): PairDayData {
+export function updatePairDayData(pair: Pair, event: ethereum.Event): PairDayData {
   let timestamp = event.block.timestamp.toI32()
   let dayID = timestamp / 86400
   let dayStartTimestamp = dayID * 86400
@@ -60,7 +60,7 @@ export function updatePairDayData(pair: Pair, event: EthereumEvent): PairDayData
   return pairDayData as PairDayData
 }
 
-export function updatePairHourData(pair: Pair, event: EthereumEvent): PairHourData {
+export function updatePairHourData(pair: Pair, event: ethereum.Event): PairHourData {
   let timestamp = event.block.timestamp.toI32()
   let hourIndex = timestamp / 3600 // get unique hour within unix history
   let hourStartUnix = hourIndex * 3600 // want the rounded effect
@@ -89,7 +89,7 @@ export function updatePairHourData(pair: Pair, event: EthereumEvent): PairHourDa
   return pairHourData as PairHourData
 }
 
-export function updateTokenDayData(token: Token, event: EthereumEvent, bundle: Bundle): TokenDayData {
+export function updateTokenDayData(token: Token, event: ethereum.Event, bundle: Bundle): TokenDayData {
   let timestamp = event.block.timestamp.toI32()
   let dayID = timestamp / 86400
   let dayStartTimestamp = dayID * 86400
--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-const */
-import { log, BigInt, BigDecimal, Address, EthereumEvent } from '@graphprotocol/graph-ts'
+import { log, BigInt, BigDecimal, Address, ethereum } from '@graphprotocol/graph-ts'
 import { ERC20 } from '../types/Factory/ERC20'
 import { ERC20SymbolBytes } from '../types/Factory/ERC20SymbolBytes'
 import { ERC20NameBytes } from '../types/Factory/ERC20NameBytes'
@@ -156,7 +156,7 @@ export function createUser(address: Address): void {
   }
 }
 
-export function createLiquiditySnapshot(position: LiquidityPosition, event: EthereumEvent): void {
+export function createLiquiditySnapshot(position: LiquidityPosition, event: ethereum.Event): void {
   let timestamp = event.block.timestamp.toI32()
   let bundle = Bundle.load('1')
   let pair = Pair.load(position.pair)
